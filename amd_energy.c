amd/amd_energy

commit c9e85067ba95b31a638fb1d5ffbd94f1145446e3

amd_energy: Conditionally check for x86_max_cores
x86_max_cores variable is removed from cpuinfo_x86 structure
and topology_num_threads_per_package() is provided to get
maximum threads per package. Add a check for that.

amd_energy.c
@@ -23,6 +23,8 @@
 #include <linux/topology.h>
 #include <linux/types.h>
 
+#include <generated/uapi/linux/version.h>
+
 #define DRV_MODULE_DESCRIPTION	"AMD energy driver"
 #define DRV_MODULE_VERSION	"1.0"
 
@@ -216,7 +218,6 @@ static int amd_create_sensor(struct device *dev,
 			     enum hwmon_sensor_types type, u32 config)
 {
 	struct hwmon_channel_info *info = &data->energy_info;
-	struct cpuinfo_x86 *c = &boot_cpu_data;
 	struct sensor_accumulator *accums;
 	int i, num_siblings, cpus, sockets;
 	u32 *s_config;
@@ -235,7 +236,12 @@ static int amd_create_sensor(struct device *dev,
 	 * c->x86_max_cores is the linux count of physical cores
 	 * total physical cores/ core per socket gives total number of sockets.
 	 */
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(6, 8, 0)
+	struct cpuinfo_x86 *c = &boot_cpu_data;
 	sockets = cpus / c->x86_max_cores;
+#else
+	sockets = cpus / topology_num_cores_per_package();
+#endif
 
 	s_config = devm_kcalloc(dev, cpus + sockets + 1,
 				sizeof(u32), GFP_KERNEL);

commit ab3466e22fe38e2bf75f06a0f76cbaf1b42662e2

amd_energy: Fix version check for 6.8.0 -> 6.9.0
The patches this check was added for ended up landing in v6.9 instead of
v6.8, so this fixes the DKMS build on e.g. Ubuntu 24.04 LTS.

Closes #11

Fixes: c9e85067ba95 ("amd_energy: Conditionally check for x86_max_cores")
Signed-off-by: Valtteri Koskivuori <vkoskiv@gmail.com>

amd_energy.c
@@ -236,7 +236,7 @@ static int amd_create_sensor(struct device *dev,
 	 * c->x86_max_cores is the linux count of physical cores
 	 * total physical cores/ core per socket gives total number of sockets.
 	 */
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(6, 8, 0)
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(6, 9, 0)
 	struct cpuinfo_x86 *c = &boot_cpu_data;
 	sockets = cpus / c->x86_max_cores;
 #else